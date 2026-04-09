4e4aa68efd2afa7053cf66ed04f44a3845c4ca3b
Anurag Kumar <anuragyadav9971456971@gmail.com>
2026-04-09 12:11:59 +0530

    added count of digits in a number

---
+++ numbers/countdigit.py
@@ -0,0 +1,12 @@
+n = int(input("enetr number :"))
+def countdigit(n):
+    if n ==0:
+        return 1
+     
+    count=0
+    while(n>0):
+        n = n//10
+        count +=1
+        
+    return count
+print(countdigit(n))    
