b37455a436b89f6860803bf34517a258850224b0
Anurag Kumar <anuragyadav9971456971@gmail.com>
2026-04-17 15:39:35 +0530

    added two and three digit number

---
+++ conditional statement/two_or_three digit.py
@@ -0,0 +1,7 @@
+num = int(input("enetr number "))
+if(10<num<=99):
+    print("this is two digit number")
+elif(99<num<=999):
+    print("this is three digit number")
+else:
+    print("this is neighter two digit nor three digit number ")        
