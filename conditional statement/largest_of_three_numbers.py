47cc5bf2c49ce17298ae1d23810f829e24dc89e6
Anurag Kumar <anuragyadav9971456971@gmail.com>
2026-04-17 12:04:03 +0530

    added largest of three no.

---
+++ conditional statement/largest_of_three_numbers.py
@@ -0,0 +1,11 @@
+num1 = int(input("enetr nummber num1 "))
+num2= int(input("enetr nummber num2 "))
+num3= int(input("enetr nummber num3 "))
+if(num1>num2):
+    print("num1 is greater",num1)
+elif(num2>num3):
+    print("num2 is greater",num2)
+elif(num3>num1):
+    print("num3 is graeter ",num3)
+else:
+    print("all are equal")
