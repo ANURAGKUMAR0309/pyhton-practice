7906686b1e9de7bf496b049cef8ded68bb520737
Anurag Kumar <anuragyadav9971456971@gmail.com>
2026-04-17 12:08:30 +0530

    added divisiblity by 5

---
+++ conditional statement/divisiblity.py
@@ -0,0 +1,7 @@
+num = int(input("enter number "))
+if (num==0):
+    print("invalid number")
+elif (num%5==0):
+    print("the number is divisible by 5 ")
+else:
+    print("not divisible by 5")    
