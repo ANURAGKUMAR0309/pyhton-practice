be6e210ac9613c0e93bee4e4d6bf29f289c1a4b1
Anurag Kumar <anuragyadav9971456971@gmail.com>
2026-04-17 11:41:47 +0530

    added to find greater number

---
+++ conditional statement/greater_number.py
@@ -0,0 +1,8 @@
+A =int(input("enter number A "))
+B=int(input("enter number B "))
+if(A>B):
+    print("A is greater number",A)
+elif(B>A):
+    print("B is greater number",B)
+else:
+    print("both are equal number")    
