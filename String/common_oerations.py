3b028d4fdd51c22b1bc84b83d8291d786896e069
Anurag Kumar <anuragyadav9971456971@gmail.com>
2026-04-26 13:20:05 +0530

    added basic operations of string

---
+++ String/common_oerations.py
@@ -0,0 +1,28 @@
+# length
+str = " i love pyhton"
+print(len(str))
+
+# indexing
+str = "ANURAG YADAV"
+print(str[0:3])
+
+# conctenation
+str = "ANURAG" + " " + "YADAV"
+print(str)
+
+# capitalize
+str = "ani"
+print(str.capitalize())
+
+
+#finding words
+str  = "ANURAG"
+print(str.find("R"))
+
+# endswith
+str = " i am a coder"
+print(str.endswith("der"))
+
+# count
+str = " i am a b.tech stduent"
+print(str.count("am"))
